e0b2f90b44b3dc311cc90c87b8b4ba5892e8dd26
AlmazKh <hamedzhanovalmaz@gmail.com>
2020-04-06 23:21:56 +0300

    Add enc and dec with random polynom parameters

--- PolynomialEncryption/src/ru/itis/Encryptor.java
+++ PolynomialEncryption/src/ru/itis/Encryptor.java
@@ -9,35 +9,26 @@ import java.util.Scanner;
 
 public class Encryptor {
 
-    /*int n = new Random().nextInt(5) + 3;
+    int n = new Random().nextInt(5) + 3;
     List<Integer> primeNumbers = new ArrayList<>(createPrimeNumbers());
     List<Integer> listOfA = new ArrayList<>();
-    int p = primeNumbers.get(new Random().nextInt(primeNumbers.size()));*/
+    int p = primeNumbers.get(new Random().nextInt(primeNumbers.size()));
 
-    int n = 3;
-    List<Integer> listOfA = new ArrayList<>();
-    int p = 991;
-
-    public void encrypt(String fileName) throws Exception{
+    public void encrypt(String fileName) throws Exception {
         FileInputStream inputStream = new FileInputStream(fileName);
         Scanner scanner = new Scanner(inputStream);
         FileWriter file = new FileWriter("encrypt.txt");
 
-        /*listOfA.add(1);
+        listOfA.add(1);
         for (int i = 0; i < n; i++) {
             listOfA.add(new Random().nextInt(4) + 1);
-        }*/
-        listOfA.add(1);
-        listOfA.add(2);
-        listOfA.add(3);
-        listOfA.add(4);
+        }
 
         StringBuilder word = new StringBuilder();
         while (scanner.hasNext()) {
-           word.append(scanner.next()).append("\n");
+            word.append(scanner.next()).append("\n");
         }
         char[] word_char = word.toString().toCharArray();
-        List<Integer> code = new ArrayList<>();
         for (int i = 0; i < word_char.length; i++) {
             int char_numb = (int) word_char[i] - 1039;
             int y = 0;
@@ -46,35 +37,31 @@ public class Encryptor {
             }
             y += listOfA.get(n);
             y = y % p;
-            code.add(y);
             file.write(y + " ");
         }
         file.close();
     }
 
     public void decrypt(String fileName) throws Exception {
-        //696 = x^3 + a1*x^2 + a2*x + a3 (mod p)
-        //v*991 + 696 = x^3 + a1*x^2 + a2*x + a3
-        //
         FileInputStream inputStream = new FileInputStream(fileName);
         Scanner scanner = new Scanner(inputStream);
         FileWriter file = new FileWriter("decrypt.txt");
         while (scanner.hasNext()) {
             int let = scanner.nextInt();
-            if(let == -562) {
+            if (let < 0) {
                 file.write("\n");
                 continue;
             }
 
-            for(int letter = 1; letter < 34; letter++) {
+            for (int letter = 1; letter < 34; letter++) {
                 int y = 0;
                 for (int j = n; j > 0; j--) {
                     y += (int) Math.pow(letter, j) * listOfA.get(n - j);
                 }
                 y += listOfA.get(n);
                 y = y % p;
-                if(y == let) {
-                    file.write((char)(letter + 1039));
+                if (y == let) {
+                    file.write((char) (letter + 1039));
                     break;
                 }
             }
